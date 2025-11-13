66c6a7173783d9b577f71bf57bae71567df98c94
vipul nilesh <vipulnilesh76@gmail.com>
2025-11-13 21:01:58 +0530

    Added api list in the controller

--- src/main/java/com/moneyanalyzer/controller/CategoryController.java
+++ src/main/java/com/moneyanalyzer/controller/CategoryController.java
@@ -4,4 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+    /*
+    Method	Endpoint	Description
+GET	/api/categories	List all categories (global + user)
+GET	/api/categories/{id}	Get category by ID
+POST	/api/categories	Create new category
+PUT	/api/categories/{id}	Update category
+DELETE	/api/categories/{id}	Delete category
+     */
+
+
+
 }
